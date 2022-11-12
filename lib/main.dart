2371224ede89518d3f388dc3f951c962e823196b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-12 16:42:54 -0600

    Added place and nav bar functionality

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ List<EventData> eventDataList = [];
 List<TeamData> teamList = [];
 bool useDefaults = true;
 
+EventData currentEventData = defaultEventData[0];
+
 void main() {
   runApp(const MyApp());
 }
@@ -49,6 +51,11 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue
+          ),
+        ),
       ),
       home: const EventPage(
           title: 'Swimscore'
@@ -77,6 +84,12 @@ class EventPage extends StatefulWidget {
 
 class _EventState extends State<EventPage> {
   int _selectedIndex = 0;
+  final List<Widget> _children = [
+    EventList(title: "Events"),
+    Teams(),
+    Settings(),
+    Visualizer()
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -99,10 +112,10 @@ class _EventState extends State<EventPage> {
         title: Text(widget.title),
         centerTitle: true,
       ),
-      body: const Center(
+      body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: EventList(title: 'Events',),
+        child: _children[_selectedIndex],
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
@@ -132,6 +145,48 @@ class _EventState extends State<EventPage> {
   }
 }
 
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Settings");
+  }
+}
+
+class Teams extends StatefulWidget {
+  const Teams({super.key});
+
+  @override
+  State<Teams> createState() => _TeamsState();
+}
+
+class _TeamsState extends State<Teams> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Teams");
+  }
+}
+
+class Visualizer extends StatefulWidget {
+  const Visualizer({super.key});
+
+  @override
+  State<Visualizer> createState() => _VisualizerState();
+}
+
+class _VisualizerState extends State<Visualizer> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Visualizer");
+  }
+}
+
 class EventList extends StatefulWidget {
   const EventList({super.key, required this.title});
 
@@ -142,12 +197,11 @@ class EventList extends StatefulWidget {
 }
 
 class _EventListState extends State<EventList> {
+  // Display the list of events
   int items = eventDataList.length;
-  void _eventPressed() {
-    /*setState(() {
-
-    });*/
-    Navigator.push(context, MaterialPageRoute(builder: (context) => const PlaceList()));
+  void _eventPressed(int i) {
+    currentEventData = eventDataList[i];
+    Navigator.push(context, MaterialPageRoute(builder: (context) => PlaceList()));
   }
   @override
   Widget build(BuildContext context) {
@@ -156,7 +210,7 @@ class _EventListState extends State<EventList> {
         itemCount: items,
         itemBuilder: (context, i) {
           return ElevatedButton(
-              onPressed: _eventPressed,
+              onPressed: () => _eventPressed(i),
               child: Text(eventDataList[i].distance.toString() + " " + eventDataList[i].name),
           );
         }
@@ -164,21 +218,89 @@ class _EventListState extends State<EventList> {
   }
 }
 
-class PlaceList extends StatelessWidget {
+class PlaceList extends StatefulWidget {
   const PlaceList({super.key});
+
+  @override
+  State<PlaceList> createState() => _PlaceListState();
+}
+
+class _PlaceListState extends State<PlaceList> {
+  // Display the list of places
+
+  void onPlacePressed(int index) {
+    // Display the team selector
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Center(
+          child: Container(
+            //constraints: BoxConstraints.expand(width: 500, height: 400),
+            padding: const EdgeInsets.all(50),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.lightBlue,
+            ),
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: teamList.length+1,
+                itemBuilder: (context, i) {
+                  if (i == 0) {
+                    return DefaultTextStyle(
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Arial',
+                          fontSize: 28,
+                        ),
+                        child: Text(index.toString(), textAlign: TextAlign.center,),
+                    );
+                  }
+                  else {return ElevatedButton(
+                      onPressed: () => onTeamPressed(index, i-1),
+                      child: Text(teamList[i-1].name),
+                  );}
+                }
+            ),
+          ),
+        );
+      }
+    );
+  }
+
+  void onTeamPressed(int placeIndex, int teamIndex) {
+    currentEventData.placeTeams[placeIndex-1] = teamList[teamIndex];
+    Navigator .of(context, rootNavigator: true).pop();
+    setState(() {});
+  }
 
   @override
   Widget build (BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Places'), //TODO: Make it display event and distance data
+        title: Text(currentEventData.distance.toString() + " " + currentEventData.name),
       ),
       body: Center(
         child: ListView.builder(
           padding: const EdgeInsets.all(30),
-          itemCount: 8, //TODO: Make dynamic with events
+          itemCount: currentEventData.places,
           itemBuilder: (context, i) {
-            return Text("Placeholder");
+            ElevatedButton eb;
+            if (currentEventData.placeTeams[i].name == "None") {
+              eb = ElevatedButton(
+                //This is the list of places
+                onPressed: () => onPlacePressed(i + 1),
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+                child: Text((i + 1).toString() + ": "),
+              );
+            } else {
+              eb = ElevatedButton(
+                //This is the list of places
+                  onPressed: () => onPlacePressed(i + 1),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: Text((i + 1).toString() + ": " + currentEventData.placeTeams[i].name),
+              );
+            }
+            return eb;
           },
         ),
       ),
@@ -190,21 +312,19 @@ class EventData {
   // Designed to hold all the data for each individual event, and to be accessible by everything that needs it
   int distance = -1;
   String name = 'none';
-  int places = 0;
+  int places = 8;
   bool isRelay = false;
-  List<String> placeTeams = []; // The order of the list is the order of the teams that score i.e. Kirkwood, Kirkwood, Webster, Ladue...
+  List<TeamData> placeTeams = List.filled (8, TeamData('None'), growable: true); // The order of the list is the order of the teams that score i.e. Kirkwood, Kirkwood, Webster, Ladue... TODO: Set list length to variable
   List<int> placesValues = []; // The point value of each placement i.e. 6, 5, 4, 3, 2, 1
 
-  EventData(this.distance, this.name, this.places, this.isRelay) {
-    //placeTeams.length = places;
-    //placesValues.length = places;
-  }
+  EventData(this.distance, this.name, this.places, this.isRelay);
 }
 
 class TeamData {
   // Designed to hold all the data for each individual team
   String name = 'none';
   int score = 0;
+  //TODO: Make it so you can pick a color for your team and when you click a place the button turns that color
 
   TeamData(this.name);
 }
